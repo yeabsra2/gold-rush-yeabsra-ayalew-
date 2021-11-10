3ea1f805ccb279bb0bc482aaaba3d03ed3081ca6
Yeabsra Ayalew <92882893+yeabsra2@users.noreply.github.com>
2021-11-10 01:07:35 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,11 +5,12 @@
  * 
  * The purpose of this code is to make a game called gold rush in which you hunt for gold as a miner on a micro bit using Makecode
  */
+music.playMelody("C5 G B A F A C5 B ", 120)
 let miner = game.createSprite(2, 2)
 let goldNugget = game.createSprite(randint(0, 4), randint(0, 4))
 let score = 0
 while (score < 10) {
-    if (miner.get(LedSpriteProperty.X) == goldNugget.get(LedSpriteProperty.X) && miner.get(LedSpriteProperty.X) == goldNugget.get(LedSpriteProperty.X)) {
+    if (miner.get(LedSpriteProperty.X) == goldNugget.get(LedSpriteProperty.X) && miner.get(LedSpriteProperty.Y) == goldNugget.get(LedSpriteProperty.Y)) {
         score += 1
         goldNugget.set(LedSpriteProperty.X, randint(0, 4))
         goldNugget.set(LedSpriteProperty.Y, randint(0, 4))
@@ -23,15 +24,12 @@ while (score < 10) {
         basic.pause(100)
     }
     if (input.acceleration(Dimension.Y) > 0) {
-        miner.change(LedSpriteProperty.X, 1)
+        miner.change(LedSpriteProperty.Y, 1)
         basic.pause(100)
     }
     if (input.acceleration(Dimension.X) < 0) {
-        miner.change(LedSpriteProperty.X, -1)
+        miner.change(LedSpriteProperty.Y, -1)
         basic.pause(100)
     }
 }
 basic.showString("Game Over")
-basic.forever(function () {
-	
-})
